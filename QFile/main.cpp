76652b432e0bad695e1ed52bf7e02598a3b0fa04
Ivan Lee <leeivan1007@gmail.com>
2018-05-09 10:00:32 +0800

    Add files via upload

---
+++ QFile/main.cpp
@@ -0,0 +1,52 @@
+#include <QCoreApplication>
+#include <QFile>
+#include <QString>
+#include <QDebug>
+#include <QTextStream>
+
+void Write(QString Filename)
+{
+    QFile mFile(Filename);
+
+    if(!mFile.open(QFile::WriteOnly | QFile::Text))
+    {
+        qDebug() << "could not open file for writting";
+        return;
+    }
+
+    QTextStream out(&mFile);
+    out << "Hello world";
+
+    mFile.flush();
+    mFile.close();
+}
+
+void Read(QString Filename)
+{
+    QFile mFile(Filename);
+
+    if(!mFile.open(QFile::ReadOnly | QFile::Text))
+    {
+        qDebug() << "could not open file for reading";
+        return;
+    }
+
+    QTextStream in(&mFile);
+    QString mText = in.readAll();
+
+    qDebug() << mText;
+
+    mFile.close();
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QString mFilename = "C:\\Users\\User\\text.txt";
+
+    Write(mFilename);
+    Read(mFilename);
+
+    return a.exec();
+}
